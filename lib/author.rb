712ba0242eb97f4b270d9dcc718e400c256bbbb9
Megan Collier <meganraec@live.com>
2018-05-15 03:10:58 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -5,11 +5,3 @@ class Author
     @name = name
   end
 end
-
-# describe "Author" do
-#   it "has a name" do
-#     author = Author.new
-#     author.name = "Uncle Bob"
-#     expect(author.name).to eq("Uncle Bob")
-#   end
-# end
